cd34e54d5442db15cfe8c2ca0ae5020ec22fb41f
Aristote02 <arisdilombadev12@gmail.com>
2024-08-19 16:45:54 +0300

    implemented document api

---
+++ Innoclinic.Documents/Documents.BusinessLogic/Configurations/BlobStorageConfigurations.cs
@@ -0,0 +1,27 @@
+﻿namespace Documents.BusinessLogic.Configurations;
+
+/// <summary>
+/// Represents the configuration settings for Blob Storage
+/// </summary>
+public class BlobStorageConfigurations
+{
+	/// <summary>
+	/// Gets or initializes the connection string for the Blob Storage account
+	/// </summary>
+	public required string ConnectionString { get; init; }
+
+	/// <summary>
+	/// Gets or initializes the name of the Blob Storage container
+	/// </summary>
+	public required string ContainerName { get; init; }
+
+	/// <summary>
+	/// Gets or initializes the primary key for accessing the Blob Storage account
+	/// </summary>
+	public required string PrimaryKey { get; init; }
+
+	/// <summary>
+	/// Gets or initializes the name of the Blob Storage account
+	/// </summary>
+	public required string AccountName { get; init; }
+}
